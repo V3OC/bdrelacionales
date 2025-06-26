72916bb879401843bbff75db370eada86878c841
V3OC <veoc09.28@gmail.com>
2025-06-25 19:40:01 -0700

    25/06/2025 - ApiProyecto - Api_Gimnasio-Socios
    Profe lo estoy volviendo a subir por que no se subieron todos los .php y evidencias en la escuela, segun yo si los habia subido, pero ahortia que cheque en mi casa me di cuenta que solo se subio un .php, tambien ahortia que estoy escribiendo esto, estoy subiendo otra ves la carpeta pero no se por que salen doble el contenido si en mi compu solo tengo uno de cada uno, espero que eso no genere problemas y si es asi me podria avisar por favor, y ver como se podria solucionar.

---
+++ Api_Gimnasio-Socios/Api_Gimnasio-Socios/getSocios.php
@@ -0,0 +1,42 @@
+<?php 
+  
+
+  $host = 'localhost';
+  $user = 'root';
+  $pass = '';
+  $dbname = 'gimnasio';
+
+    $conn = new mysqli($host, $user, $pass, $dbname);
+    
+    if($conn->connect_error){
+        http_response_code(500);
+        echo json_encode(['error' => 'Error de conexion: ' .$conn->connect_error]);
+        exit;
+
+
+
+    }
+          $sql = "SELECT * FROM socios";
+          $result = $conn->query($sql);
+
+          $actors = [];
+
+
+
+
+          if($result && $result->num_rows > 0);{
+            while($row = $result->fetch_assoc()){
+               $actors[] = $row;
+
+            }
+
+        }
+        $conn->close();
+
+        header("content-type: application/json");
+        echo json_encode($actors);
+
+    //https://localhost/api/getActors.php
+    
+
+?>
